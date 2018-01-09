29a2155a6c5b96f6b8be7866decb9640bd9c5142
Harjot Singh <harjot@harkul.com>
2018-01-09 22:15:46 +0000

    Add backend entry point
    - Import logger
    - Wrap main function as an `async` function to allow top-level `await`

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,1 +1,15 @@
-console.log( 'Hi' )
+import logger from './lib/logger'
+
+/**
+ * Async entry point for application.
+ */
+async function main() {
+  logger.info( 'Starting...' )
+}
+
+
+// Handle any errors by crashing
+main.catch( error => {
+  logger.error( error )
+  process.exit( 1 )
+} )
