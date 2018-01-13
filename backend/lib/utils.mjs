1b7b1ea3cc5ddfbde5324ca034b9623b94747d79
Harjot Singh <harjot@harkul.com>
2018-01-13 05:51:36 +0000

    backend: wrap callbacks in promisifies

--- backend/lib/utils.mjs
+++ backend/lib/utils.mjs
@@ -3,39 +3,13 @@
  * @ignore
  */
 
-import path from 'path'
+import fs from 'fs'
+import util from 'util'
 
-import download from 'download'
-import ProgressBar from 'progress'
+const { writeFile, readFile, rename } = fs
+const { promisify } = util
 
-/**
- * Returns the download URL for a database release on github.
- * @param version The version to retrieve the download URL for
- */
-export const dbDownloadURL = version =>
-  `https://github.com/ShabadOS/database/releases/tag/${version}/database.sqlite`
-
-/**
- * Download file to destination, with a progress bar.
- * @param url The target URL
- * @param destination The destination (folder)
- */
-export const downloadFile = async ( url, destination ) => {
-  const filename = path.basename( url )
-
-  // Setup progress bar
-  const bar = new ProgressBar( `${filename} [:bar] :percent :etas`, {
-    complete: '=',
-    incomplete: ' ',
-    width: 100,
-    total: 0,
-  } )
-
-  // Return download promise, as well as configuring the progress bar
-  return download( url, destination )
-    .on( 'response', res => {
-      // Tick the bar when data is received
-      bar.total = res.headers[ 'content-length' ]
-      res.on( 'data', ( { length } ) => bar.tick( length ) )
-    } )
-}
+// Promise wrappers around callback I/O functions
+export const writeFileAsync = promisify( writeFile )
+export const readFileAsync = promisify( readFile )
+export const renameAsync = promisify( rename )
