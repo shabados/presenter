7afc6ee24d98f5a3653e52d114004bb4bf3446e3
Harjot Singh <harjot@harkul.com>
2018-01-13 22:27:26 +0000

    Import Shabad OS database and update project metadata
    - Fix README extension
    - Add fields to package.json
    - Rename package.json to reflect scoped package names
    - Add `@shabados/database` package
    - Fix typos

---
+++ backend/lib/database.mjs
@@ -0,0 +1,18 @@
+/**
+ * Collection of updater functions.
+ * @ignore
+ */
+
+
+import settings from './settings'
+import logger from './logger'
+
+
+class Database {
+  constructor() {
+  }
+
+}
+
+// Export database instance singleton
+export default new Database()
