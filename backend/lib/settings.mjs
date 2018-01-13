0c2820c0edbd2f7c37a61a233f01d1ff2a76f299
Harjot Singh <harjot@harkul.com>
2018-01-13 19:59:01 +0000

    Enable CJS backend named ES6 imports

--- backend/lib/settings.mjs
+++ backend/lib/settings.mjs
@@ -3,29 +3,89 @@
  * @ignore
  */
 
-import fs from 'fs'
-
-import nconf from 'nconf'
-
-import defaultSettings from '../settings.default'
+import { readJsonSync, writeJsonSync } from 'fs-extra'
 
 import logger from './logger'
 
-const SETTINGS_FILE = '../settings.json'
-const DEFAULT_SETTINGS_FILE = '../settings.default.json'
+const SETTINGS_FILE = './settings.json'
+const DEFAULT_SETTINGS_FILE = './settings.default.json'
 
-logger.info( 'Loading settings' )
+/**
+ * Simple class to manage application settings
+ */
+class Settings {
+  constructor() {
+    this.settings = {}
 
-// // Instantiate settings if they don't exist
-if ( !fs.existsSync( SETTINGS_FILE ) ) {
-  fs.writeFileSync( SETTINGS_FILE, JSON.stringify( defaultSettings, null, 2 ) )
+    this.loadSettings()
+  }
+
+  /**
+   * Loads settings, merging them with the defaults.
+   */
+  loadSettings() {
+    logger.info( 'Loading settings' )
+
+    // Load both settings files
+    const defaultSettings = readJsonSync( DEFAULT_SETTINGS_FILE )
+    const settings = Settings.checkCreateSettings()
+
+    // Merge and store them
+    this.settings = { ...defaultSettings, ...settings }
+
+    // Save them for good measure
+    this.saveSettings()
+  }
+
+  /**
+   * Saves the settings back to disk.
+   */
+  saveSettings() {
+    writeJsonSync( SETTINGS_FILE, this.settings, { spaces: 2 } )
+  }
+
+  /**
+   * Returns all settings if no parameters, else the path provided.
+   */
+  get( path ) {
+    return path
+      ? path.split( '.' ).reduce( ( settings, key ) => settings[ key ], this.settings )
+      : this.settings
+  }
+
+  /**
+   * Sets a key-value pair and saves.
+   * @param key The key to set the value for
+   * @param value The new value of the key
+   */
+  set( key, value ) {
+    this.settings[ key ] = value
+    this.saveSettings()
+  }
+
+  /**
+   * Merges a given settings object with the current settings and saves.
+   * @param settings The settings object to merge with
+   */
+  merge( settings ) {
+    this.settings = { ...this.settings, ...settings }
+    this.saveSettings()
+  }
+
+  /**
+   * Creates a settings.json file if it doesn't already exist, or is corrupt.
+   */
+  static checkCreateSettings() {
+    // If we can't read the JSON file, recreate it
+    try {
+      return readJsonSync( SETTINGS_FILE )
+    } catch ( err ) {
+      logger.warn( 'Settings file is corrupt or non-existent. Recreating.' )
+      writeJsonSync( SETTINGS_FILE, {} )
+      return {}
+    }
+  }
 }
 
-// Load settings with nconf, with priority on settings.json
-nconf
-  .file( 'settings', SETTINGS_FILE )
-  .file( 'defaultSettings', DEFAULT_SETTINGS_FILE )
-
-nconf.save()
-
-export default nconf
+// Export the instance
+export default new Settings()
---
+++ backend/lib/utils.mjs
@@ -0,0 +1,4 @@
+/**
+ * Collection of utility functions.
+ * @ignore
+ */
