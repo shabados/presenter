6a656953cba702d04e3cd1840c18d81e475d7d21
Harjot Singh <harjot@harkul.com>
2018-01-13 20:53:46 +0000

    Create backend constants file
    - Refactor constants from backend settings

---
+++ backend/lib/consts.mjs
@@ -0,0 +1,17 @@
+/**
+ * Constants file.
+ * @ignore
+ */
+
+// Check every 15 seconds (in dev)
+export const UPDATE_CHECK_INTERVAL = 1000 * 15
+
+// Setting file locations
+export const SETTINGS_FILE = './settings.json'
+export const DEFAULT_SETTINGS_FILE = './settings.default.json'
+
+// Temporary files directory
+export const TEMP_DIR = 'tmp'
+
+// ShabadOS database path
+export const DB_PATH = 'database.sqlite'
