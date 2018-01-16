726c92c599e0d4fe2e1ccd04e98239992813e64e
Harjot Singh <harjot@harkul.com>
2018-01-16 01:08:05 +0000

    backend: use the new sockets and sessionmanager

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,8 +1,8 @@
 import updateNotifier from 'update-notifier'
 
 import { setupExpress } from './lib/express'
-import { setupWebsocket } from './lib/sockets'
-import database from './lib/database'
+import SessionManager from './lib/SessionManager'
+import Socket from './lib/Sockets'
 
 import logger from './lib/logger'
 
@@ -20,8 +20,12 @@ async function main() {
   // Setup the express server with websockets
   const server = await setupExpress()
 
-  // Setup the WebSocket server, attaching it to the HTTP instance
-  const socket = setupWebsocket( server )
+  // Setup the websocket server
+  const socket = new Socket( server )
+
+  // Setup the session manager on top of the Socket instance
+  // eslint-disable-next-line
+  const sessionManager = new SessionManager( socket )
 
   // Start the server
   const port = process.env.PORT || 8080
