ff4425ff654da5693e53597afe60910960f93db7
Harjot Singh <harjot@harkul.com>
2018-01-20 03:58:51 +0000

    Create `Navigator` component
    This component houses all the other components used to use Shabad OS,
    in the bottom-right corner of the screen.

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+
+import 'typeface-noto-sans'
+import Reboot from 'material-ui/Reboot'
+
+import Navigator from './components/Navigator'
+
 import './App.css'
 
 class App extends Component {
+  constructor( props ) {
+    super( props )
+
+    this.state = {
+      settings: null,
+      session: null,
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <Reboot />
+        <Navigator></Navigator>
       </div>
     )
   }
