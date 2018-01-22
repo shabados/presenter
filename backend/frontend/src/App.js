40690a5e643b7004fa86b1cd62e1dcaf558fca72
Harjot Singh <harjot@harkul.com>
2018-01-22 19:29:05 +0000

    frontend: some boilerplate

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 
 import 'typeface-noto-sans'
-
 import Reboot from 'material-ui/Reboot'
 
 import Navigator from './components/Navigator'
@@ -9,12 +8,20 @@ import Navigator from './components/Navigator'
 import './App.css'
 
 class App extends Component {
+  constructor( props ) {
+    super( props )
+
+    this.state = {
+      settings: null,
+      session: null,
+    }
+  }
+
   render() {
     return (
       <div className="app">
         <Reboot />
         <Navigator></Navigator>
-        <div>WHY the shaadow</div>
       </div>
     )
   }
