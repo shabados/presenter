a139a585f85b95a3488a4458bb0a563e5b0eacd3
Harjot Singh <harjot@harkul.com>
2018-03-23 21:15:18 +0000

    frontend: convert ASCII to unicode on search input

--- backend/frontend/src/components/Navigator/Search.js
+++ backend/frontend/src/components/Navigator/Search.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import { Input } from 'material-ui'
+import { toUnicode } from '@shabados/gurmukhi-utils'
 
 import './Search.css'
 
-const Search = () => {
-  return (
-    <div className="search">
-      <Input className="input" placeholder="ਖੋਜ" disableUnderline autoFocus />
-    </div>
-  )
+/**
+ * Search Component.
+ * Converts ASCII to unicode on input.
+ * Displays results.
+ */
+class Search extends Component {
+  constructor( props ) {
+    super( props )
+
+    this.state = {
+      search: '',
+    }
+  }
+
+  onChange = ( { target: { value } } ) => this.setState( { search: toUnicode( value ) } )
+
+  render() {
+    const { search } = this.state
+
+    return (
+      <div className="search">
+        <Input
+          className="input"
+          onChange={this.onChange}
+          value={search}
+          placeholder="ਖੋਜ"
+          disableUnderline
+          autoFocus
+        />
+      </div>
+    )
+  }
+
 }
 
 export default Search
