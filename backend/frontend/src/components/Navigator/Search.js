344135ae2b91e70a8b35b1cd2a786fd26924cfd4
Harjot Singh <harjot@harkul.com>
2018-03-23 15:30:05 +0000

    frontend: add search bar and hover

---
+++ backend/frontend/src/components/Navigator/Search.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Input } from 'material-ui'
+
+import './Search.css'
+
+const Search = () => {
+  return (
+    <div className="search">
+      <Input className="input" placeholder="ਖੋਜ" />
+    </div>
+  )
+}
+
+export default Search
