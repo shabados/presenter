83335626ea15ce07aca13622b8bce8ddd6ab47dd
Harjot Singh <harjot@harkul.com>
2018-03-24 18:21:19 +0000

    frontend: add constants

--- backend/frontend/src/lib/consts.js
+++ backend/frontend/src/lib/consts.js
@@ -3,4 +3,12 @@
  * @ignore
  */
 
+/* Backend Info */
+export const BACKEND_HOST = 'localhost'
+export const BACKEND_PORT = 8080
+export const URL = `http://${BACKEND_HOST}:${BACKEND_PORT}`
+export const WS_URL = `ws://${BACKEND_HOST}:${BACKEND_PORT}`
+
+/* Navigator */
 export const NAVIGATOR_URL = '/navigator'
+export const MIN_SEARCH_CHARS = 2
