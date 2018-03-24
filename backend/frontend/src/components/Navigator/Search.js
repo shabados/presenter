436b2de7ec13b5eeaed6d37c7845536e274a8c27
Harjot Singh <harjot@harkul.com>
2018-03-24 18:22:40 +0000

    frontend: basic search functionality

--- backend/frontend/src/components/Navigator/Search.js
+++ backend/frontend/src/components/Navigator/Search.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 
-import { Input } from 'material-ui'
+import { Input, List, ListItem } from 'material-ui'
 import { toUnicode } from '@shabados/gurmukhi-utils'
+
+import { MIN_SEARCH_CHARS } from '../../lib/consts'
+import controller from '../../lib/controller'
 
 import './Search.css'
 
@@ -16,13 +19,56 @@ class Search extends Component {
 
     this.state = {
       search: '',
+      results: [],
     }
   }
 
-  onChange = ( { target: { value } } ) => this.setState( { search: toUnicode( value ) } )
+  componentDidMount() {
+    controller.on( 'results', this.onResults )
+  }
+
+  componentWillUnmount() {
+    controller.off( 'results', this.onResults )
+  }
+
+  onResults = results => this.setState( { results } )
+
+  /**
+   * Run on change of value in the search box.
+   * Converts ascii to unicode if need be.
+   * Sends the search through to the controller.
+   * @param value The new value of the search box.
+   */
+  onChange = ( { target: { value } } ) => {
+    const state = {}
+    const search = toUnicode( value )
+
+    // Search if enough letters, otherwise clear results
+    if ( search.length > MIN_SEARCH_CHARS ) {
+      controller.search( search )
+    } else {
+      state[ 'results' ] = []
+    }
+
+    this.setState( { ...state, search } )
+  }
+
+  /**
+   * Renders a single result, highlighting the match.
+   * @param gurmukhi The shabad line to display.
+   * @param id The id of the line.
+   */
+  renderResult = ( { gurmukhi, id } ) => {
+    const { search } = this.state
+
+    // Split the line into first letters and check where the match is
+
+
+    return <ListItem key={id}>{gurmukhi}</ListItem>
+  }
 
   render() {
-    const { search } = this.state
+    const { search, results } = this.state
 
     return (
       <div className="search">
@@ -34,6 +80,9 @@ class Search extends Component {
           disableUnderline
           autoFocus
         />
+        <List className="results">
+          {results ? results.slice( 0, 20 ).map( this.renderResult ) : ''}
+        </List>
       </div>
     )
   }
