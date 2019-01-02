6a3523dd0b657bc532141ff58d22bb26ac136d4b
Harjot Singh <harjot@harkul.com>
2019-01-02 23:27:39 +0000

    Create temporary data folder

--- app/lib/consts.js
+++ app/lib/consts.js
@@ -7,13 +7,14 @@ import { getAppDataPath } from 'appdata-path'
 
 import { getDateFilename } from './utils'
 
-// Check every 1 minute
+// Check every 5 minutes
 export const UPDATE_CHECK_INTERVAL = 1000 * 60
 
 // Folder locations
 export const DATA_FOLDER = join( getAppDataPath(), 'Shabad OS' )
 export const CUSTOM_THEMES_FOLDER = join( DATA_FOLDER, 'themes' )
 export const HISTORY_FOLDER = join( DATA_FOLDER, 'history' )
+export const TMP_FOLDER = join( DATA_FOLDER, 'temp' )
 
 // File locations
 export const HISTORY_FILE = join( HISTORY_FOLDER, `${getDateFilename( new Date() )}.csv` )
--- app/server.js
+++ app/server.js
@@ -5,7 +5,7 @@ import SessionManager from './lib/SessionManager'
 import Socket from './lib/Sockets'
 import { searchLines, getBanis } from './lib/db'
 import logger from './lib/logger'
-import { PORT, CUSTOM_THEMES_FOLDER, DATA_FOLDER, HISTORY_FILE, HISTORY_FOLDER } from './lib/consts'
+import { PORT, CUSTOM_THEMES_FOLDER, DATA_FOLDER, HISTORY_FILE, HISTORY_FOLDER, TMP_FOLDER } from './lib/consts'
 
 /**
  * Async entry point for application.
@@ -14,7 +14,7 @@ async function main() {
   logger.info( 'Starting...' )
 
   // Check if the data directories for the app exists, otherwise create it
-  ;[ DATA_FOLDER, CUSTOM_THEMES_FOLDER, HISTORY_FOLDER ].map( ensureDirSync )
+  ;[ DATA_FOLDER, CUSTOM_THEMES_FOLDER, HISTORY_FOLDER, TMP_FOLDER ].map( ensureDirSync )
 
   // Setup the express server with WebSockets
   const mounts = [
