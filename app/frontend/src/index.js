2d67901c295a5783a55aa6eeef366a71332328e1
Harjot Singh <harjot@harkul.com>
2019-01-03 03:59:01 +0000

    Add frontend Sentry integration

--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -3,10 +3,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
+import * as Sentry from '@sentry/browser'
+
+import { SENTRY_DSN } from './lib/consts'
 
 import App from './App'
 import './index.css'
 
+// Enable Sentry if in production
+if ( process.env === 'production' ) Sentry.init( { dsn: SENTRY_DSN } )
 
 // Render the React app
 ReactDOM.render( <Router><App /></Router>, document.getElementById( 'root' ) )
