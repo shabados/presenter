abecd19820b5281cd9dcc4944ef64d20da8c15f7
Harjot Singh <harjot@harkul.com>
2019-01-03 01:06:53 +0000

    Create update temporary files folder

--- app/lib/consts.js
+++ app/lib/consts.js
@@ -15,14 +15,12 @@ export const DATA_FOLDER = join( getAppDataPath(), 'Shabad OS' )
 export const CUSTOM_THEMES_FOLDER = join( DATA_FOLDER, 'themes' )
 export const HISTORY_FOLDER = join( DATA_FOLDER, 'history' )
 export const TMP_FOLDER = join( DATA_FOLDER, 'temp' )
+export const UPDATE_TMP_FOLDER = join( TMP_FOLDER, '@shabados', 'database' )
 
 // File locations
 export const HISTORY_FILE = join( HISTORY_FOLDER, `${getDateFilename( new Date() )}.csv` )
 export const SETTINGS_FILE = join( DATA_FOLDER, 'settings.json' )
 export const DEFAULT_SETTINGS_FILE = `${__dirname}/../settings.default.json`
-
-// Temporary files directory
-export const TEMP_DIR = 'tmp'
 
 // Max Search results to return in one go
 export const MAX_RESULTS = 20
