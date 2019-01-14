a268bb344d1dbda66d7d912e2f8c3e8a8b5207cb
Harjot Singh <harjot@harkul.com>
2019-01-14 18:44:45 +0000

    Add prop types to ScreenReader

--- app/frontend/src/ScreenReader/index.js
+++ app/frontend/src/ScreenReader/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { shape, arrayOf, string } from 'prop-types'
 
 import './index.css'
 
@@ -6,10 +7,20 @@ class ScreenReader extends Component {
   render() {
     const { shabad, bani } = this.props
 
-    if ( !shabad && !bani ) { return null }
+    if ( !shabad && !bani ) return null
 
     return shabad.lines.map( ( { gurmukhi, id } ) => <p key={id}>{gurmukhi}</p> )
   }
+}
+
+ScreenReader.propTypes = {
+  shabad: shape( { lines: arrayOf( shape( { id: string, gurmukhi: string } ) ) } ),
+  bani: shape( { lines: arrayOf( shape( { id: string, gurmukhi: string } ) ) } ),
+}
+
+ScreenReader.defaultProps = {
+  shabad: undefined,
+  bani: undefined,
 }
 
 export default ScreenReader
