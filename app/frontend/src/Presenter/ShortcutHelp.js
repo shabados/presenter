fbe449c106a44ac8f569531b5104fbd1678de435
Harjot Singh <harjot@harkul.com>
2019-02-10 21:05:25 +0000

    Correct unique key for Shortcut help

--- app/frontend/src/Presenter/ShortcutHelp.js
+++ app/frontend/src/Presenter/ShortcutHelp.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { Typography, GridList } from '@material-ui/core'
 
-import { SHORTCUTS, DEFAULT_SHORTCUT_MAP } from '../lib/consts'
+import { DEFAULT_SHORTCUT_MAP } from '../lib/consts'
 
 import './ShortcutHelp.css'
 
@@ -14,7 +14,7 @@ const ShortcutHelp = () => (
     <Typography className="title" type="title">Keyboard Shortcuts</Typography>
     <GridList className="shortcuts" cols={3} cellHeight="auto">
       {Object.entries( DEFAULT_SHORTCUT_MAP ).map( ( [ name, shortcut ] ) => (
-        <dl className="shortcut" key={SHORTCUTS[ name ]}>
+        <dl className="shortcut" key={name}>
           <dt>{shortcut.join( ', ' )}</dt>
           <dd>{name}</dd>
         </dl>
