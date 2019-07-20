ea20f35dde40baf65ca35dae33a3babb1d508c3e
Harjot Singh <harjot@harkul.com>
2019-07-20 01:12:23 +0100

    fix(frontend): re-render hotkeys HOC when shabad or bani changes
    Fixes #116

--- app/frontend/src/Controller/index.js
+++ app/frontend/src/Controller/index.js
@@ -205,7 +205,7 @@ class Controller extends Component {
   onHover = hovered => this.setState( { hovered } )
 
   render() {
-    const { location, settings } = this.props
+    const { location, settings, shabad, bani } = this.props
     const { hovered } = this.state
 
     const { local: { theme: { simpleGraphics: simple } } } = settings
@@ -219,7 +219,7 @@ class Controller extends Component {
     ]
 
     return (
-      <Switch>
+      <Switch key={( shabad || bani || {} ).id}>
         {routes.map( ( [ route, Component, BarComponent ] ) => (
           <Route
             key={route}
