8aa0424e95834e54a349bd862bd6cbf20b979cf1
Harjot Singh <harjot@harkul.com>
2019-10-03 02:31:10 +0100

    refactor(frontend): move contents of index.css to App.css

--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -9,7 +9,6 @@ import * as Sentry from '@sentry/browser'
 import { SENTRY_DSN } from './lib/consts'
 
 import App from './App'
-import './index.css'
 
 // Enable Sentry if in production
 if ( process.env === 'production' ) Sentry.init( { dsn: SENTRY_DSN } )
