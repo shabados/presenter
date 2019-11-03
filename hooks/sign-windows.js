93fb2fdff7f35c2716b7d267cbfc76f45bef4751
Harjot Singh <harjot@harkul.com>
2019-11-03 04:48:52 +0000

    build: import certificate into current user's store for windows signing

--- hooks/sign-windows.js
+++ hooks/sign-windows.js
@@ -1,36 +1,37 @@
 const { execSync } = require( 'child_process' )
-const { writeFileSync, unlinkSync } = require( 'fs' )
-const { tmpdir } = require( 'os' )
 
 const {
-  WIN_CSC_LINK,
-  WIN_CSC_KEY_PASSWORD,
   SIGN_TOOL_PATH = 'C:\\Program Files (x86)\\Windows Kits\\10\\bin\\x64\\signtool.exe',
   TIMESTAMP_SERVER = 'http://timestamp.digicert.com',
 } = process.env
 
 const SITE = 'https://shabados.com'
 
-const certificate = Buffer.from( WIN_CSC_LINK, 'base64' )
+const importPfx = ( certPath, password ) => {
+  const command = [
+    [ 'certutil' ],
+    [ '-user' ],
+    [ '-f' ],
+    [ '-p', `"${password}"` ],
+    [ '-importPfx', 'My', `"${certPath}"`, 'NoRoot' ],
+  ].map( sub => sub.join( ' ' ) ).join( ' ' )
 
-exports.default = async ( { path, name } ) => {
-  if ( !WIN_CSC_LINK ) return
+  try {
+    execSync( command, { stdio: 'inherit' } )
+  } catch {
+    console.error( 'Unable to import certificate' )
+  }
+}
 
-  const isFile = WIN_CSC_LINK.includes( 'file://' )
-
-  const certPath = isFile ? WIN_CSC_LINK.replace( 'file://', '' ) : `${tmpdir()}\\cert.pfx`
-
-  if ( !isFile ) writeFileSync( certPath, certificate )
-
+const signBinary = ( path, name ) => {
   const command = [
     [ `"${SIGN_TOOL_PATH}"` ],
     [ 'sign' ],
-    [ '/sm' ],
+    [ '/a' ],
+    [ '/s', 'My' ],
     [ '/t', `"${TIMESTAMP_SERVER}"` ],
-    [ '/f', `"${certPath}"` ],
     [ '/d', `"${name}"` ],
     [ '/du', `"${SITE}"` ],
-    [ '/p', `"${WIN_CSC_KEY_PASSWORD}"` ],
     [ `"${path}"` ],
   ].map( sub => sub.join( ' ' ) ).join( ' ' )
 
@@ -39,6 +40,11 @@ exports.default = async ( { path, name } ) => {
   } catch {
     console.error( `Signing ${path} failed` )
   }
+}
 
-  if ( !isFile ) unlinkSync( certPath )
+exports.default = ( { path, name, cscInfo: { file, password } = {} } ) => {
+  if ( !file ) return
+
+  importPfx( file, password )
+  signBinary( path, name, file )
 }
