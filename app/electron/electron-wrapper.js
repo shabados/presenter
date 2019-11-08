1bac568fd10195c3ed72e17397f0eaa8837f9d95
Harjot Singh <harjot@harkul.com>
2019-11-08 23:03:59 +0000

    fix(electron): close everything on Mac if main window is closed
    Closes #289

--- app/electron/electron-wrapper.js
+++ app/electron/electron-wrapper.js
@@ -45,13 +45,7 @@ function createWindow() {
 app.on( 'ready', createWindow )
 
 // Quit when all windows are closed.
-app.on( 'window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if ( process.platform !== 'darwin' ) {
-    app.quit()
-  }
-} )
+app.on( 'window-all-closed', () => app.quit() )
 
 app.on( 'activate', () => {
   if ( mainWindow === null ) {
