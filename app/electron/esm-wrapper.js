a6c4ffd10da1201439e277b3f38815ac0271f1ba
Harjot Singh <harjot@harkul.com>
2019-12-14 03:45:11 +0000

    refactor(backend/electron): add ESM_DISABLE_CACHE to process.env for the Electron process

--- app/electron/esm-wrapper.js
+++ app/electron/esm-wrapper.js
@@ -2,6 +2,9 @@
 
 const [ ,, entry = 'entry' ] = process.argv
 
+// Disable the ESM cache
+process.env.ESM_DISABLE_CACHE = 1
+
 // Patch require to allow for ES6 imports
 require = require( 'esm' )( module )
 
