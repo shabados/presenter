fc582f3fe6d6b2720488a42d57d50d014165f66f
Harjot Singh <harjot@harkul.com>
2020-01-09 18:31:16 +0000

    fix(frontend/overlay): get translations correctly for overlay

--- app/frontend/src/Overlay/index.js
+++ app/frontend/src/Overlay/index.js
@@ -4,14 +4,15 @@ import Line from './Line'
 import ThemeLoader from './ThemeLoader'
 
 import { getTranslation, findLineIndex } from '../lib/utils'
-import { ContentContext, SettingsContext, StatusContext } from '../lib/contexts'
+import { ContentContext, SettingsContext, StatusContext, RecommendedSourcesContext } from '../lib/contexts'
 
 import './index.css'
 
 const Overlay = () => {
   const { shabad, bani, lineId } = useContext( ContentContext )
-  const { settings } = useContext( SettingsContext )
+  const settings = useContext( SettingsContext )
   const { connected } = useContext( StatusContext )
+  const recommendedSources = useContext( RecommendedSourcesContext )
 
   const { local: localSettings } = settings || {}
   const { sources } = localSettings || {}
@@ -23,7 +24,13 @@ const Overlay = () => {
   const lineIndex = findLineIndex( lines, lineId )
   const line = lineIndex > -1 ? lines[ lineIndex ] : null
 
-  const getTranslationFor = languageId => getTranslation( { shabad, sources, line, languageId } )
+  const getTranslationFor = languageId => getTranslation( {
+    shabad,
+    sources,
+    line,
+    languageId,
+    recommendedSources,
+  } )
 
   return (
     <div className="overlay">
