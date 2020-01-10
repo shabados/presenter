534e02eccf58a2610d46e7f8ebec274d048297cd
Harjot Singh <harjot@harkul.com>
2020-01-10 15:26:23 +0000

    build(ci): modify release changelog generation to all versions since last full release

--- get-changelog.sh
+++ get-changelog.sh
@@ -4,12 +4,13 @@ if [ $IS_BETA ]; then
     # If we are on a beta, only use the current version
     RELEASE_COUNT=1; 
 else
-    # Otherwise, use all the versions of the current version
+    # Otherwise, use all the versions from the previous full release to the current
     
-    # Latest version is the last non-beta/alpha we can find
-    LATEST_TAG=$(git show-ref --tags | grep -E '[0-9]+\.[0-9]+\.[0-9]+$' | tail -n 1 | awk -F / '{ print $3 }')
-    # Count the number of x.x.x releases, includin alphas and betas
-    RELEASE_COUNT=$(git show-ref --tags | grep $LATEST_TAG | wc -l)
+    # Get the release before the current
+    PREVIOUS_RELEASE_TAG=$(git show-ref --tags | grep -E '[0-9]+\.[0-9]+\.[0-9]+$' | tail -n 2 | head -n 1 | awk -F / '{ print $3 }')
+
+    # Count the number of x.x.x releases, including alphas and betas
+    RELEASE_COUNT=$(git tag --contains $PREVIOUS_RELEASE_TAG | tail -n +2 | wc -l)
 fi
 
 RELEASE_COUNT=$(($RELEASE_COUNT + 1))
