e2b0bbedd4048c52f1627166ac49c86185f09c63
Harjot Singh <harjot@harkul.com>
2020-01-31 21:56:08 +0000

    feat(frontend/presenter): add shortcuts for copying lines
    Closes #188
    Related #85

--- app/frontend/src/Presenter/Display.js
+++ app/frontend/src/Presenter/Display.js
@@ -2,13 +2,19 @@ import React, { useContext } from 'react'
 import { hot } from 'react-hot-loader/root'
 import { shape, bool } from 'prop-types'
 import classNames from 'classnames'
+import { GlobalHotKeys } from 'react-hotkeys'
+import copy from 'copy-to-clipboard'
+import { useSnackbar } from 'notistack'
 
-import { getTranslation, getTransliteration, findLineIndex } from '../lib/utils'
-import { ContentContext, RecommendedSourcesContext } from '../lib/contexts'
+import { getTranslation, getTransliteration, findLineIndex, mapPlatformKeys } from '../lib/utils'
+import { ContentContext, RecommendedSourcesContext, WritersContext } from '../lib/contexts'
+import hotkeys, { GLOBAL_SHORTCUTS } from '../lib/keyMap'
 
 import Line from './Line'
 
 import './Display.css'
+
+const truncate = input => ( input.length > 30 ? `${input.substring( 0, 30 )}...` : input )
 
 /**
  * Display Component.
@@ -38,6 +44,8 @@ const Display = ( { settings } ) => {
   const lineIndex = findLineIndex( lines, lineId )
   const line = lineIndex > -1 ? lines[ lineIndex ] : null
 
+  const writers = useContext( WritersContext )
+
   // Get the next lines
   const { nextLines: nextLineCount, previousLines: previousLineCount } = display
   const previousLines = previousLineCount && lineIndex
@@ -56,52 +64,88 @@ const Display = ( { settings } ) => {
 
   const getTransliterationFor = languageId => getTransliteration( line, languageId )
 
+
+  // Copy lines to clipboard
+  const { enqueueSnackbar } = useSnackbar()
+  const copyToClipboard = text => () => {
+    if ( !text ) return
+
+    copy( text )
+    enqueueSnackbar( `Copied "${truncate( text )}" to clipboard`, { autoHideDuration: 1000, preventDuplicate: true } )
+  }
+
+  const copyAuthor = () => {
+    if ( !line ) return
+
+    const { sourceId, writerId } = shabad || line.shabad
+    const { sourcePage } = line
+
+    const { nameEnglish: sourceName, pageNameEnglish: pageName } = recommendedSources[ sourceId ]
+    const { nameEnglish: writerName } = writers[ writerId ]
+
+    copyToClipboard( `${writerName} - ${sourceName} - ${pageName} ${sourcePage}` )()
+  }
+  const copyGurmukhi = copyToClipboard( line && line.gurmukhi )
+  const copyEnglishTransliteration = copyToClipboard( line && getTransliterationFor( 1 ) )
+  const copyEnglishTranslation = copyToClipboard( line && getTranslationFor( 1 ) )
+
+  const hotkeyHandlers = {
+    [ GLOBAL_SHORTCUTS.copyGurmukhi.name ]: copyGurmukhi,
+    [ GLOBAL_SHORTCUTS.copyEnglishTransliteration.name ]: copyEnglishTransliteration,
+    [ GLOBAL_SHORTCUTS.copyEnglishTranslation.name ]: copyEnglishTranslation,
+    [ GLOBAL_SHORTCUTS.copyAuthor.name ]: copyAuthor,
+  }
+
   return (
-    <div className={classNames( { simple, background }, 'display' )}>
-      <div className="background-image" />
-      <div className={classNames( { dim }, 'previous-lines' )}>
-        {line && previousLines.map( ( { id, gurmukhi } ) => (
-          <Line
-            key={id}
-            className="previous-line"
-            simpleGraphics={simple}
-            {...layout}
-            {...display}
-            {...vishraams}
-            gurmukhi={gurmukhi}
-          />
-        ) )}
+    <GlobalHotKeys keyMap={mapPlatformKeys( hotkeys )} handlers={hotkeyHandlers}>
+
+      <div className={classNames( { simple, background }, 'display' )}>
+        <div className="background-image" />
+        <div className={classNames( { dim }, 'previous-lines' )}>
+          {line && previousLines.map( ( { id, gurmukhi } ) => (
+            <Line
+              key={id}
+              className="previous-line"
+              simpleGraphics={simple}
+              {...layout}
+              {...display}
+              {...vishraams}
+              gurmukhi={gurmukhi}
+            />
+          ) )}
+        </div>
+        {line && (
+        <Line
+          className={classNames( { highlight }, 'current-line' )}
+          {...layout}
+          {...display}
+          {...vishraams}
+          gurmukhi={line.gurmukhi}
+          englishTranslation={display.englishTranslation && getTranslationFor( 1 )}
+          punjabiTranslation={display.punjabiTranslation && getTranslationFor( 2 )}
+          spanishTranslation={display.spanishTranslation && getTranslationFor( 3 )}
+          englishTransliteration={display.englishTransliteration && getTransliterationFor( 1 )}
+          hindiTransliteration={display.hindiTransliteration && getTransliterationFor( 4 )}
+          urduTransliteration={display.urduTransliteration && getTransliterationFor( 5 )}
+          simpleGraphics={simple}
+        />
+        )}
+        <div className={classNames( { dim }, 'next-lines' )}>
+          {line && nextLines.map( ( { id, gurmukhi } ) => (
+            <Line
+              key={id}
+              className="next-line"
+              simpleGraphics={simple}
+              {...layout}
+              {...display}
+              {...vishraams}
+              gurmukhi={gurmukhi}
+            />
+          ) )}
+        </div>
       </div>
-      {line && (
-      <Line
-        className={classNames( { highlight }, 'current-line' )}
-        {...layout}
-        {...display}
-        {...vishraams}
-        gurmukhi={line.gurmukhi}
-        englishTranslation={display.englishTranslation && getTranslationFor( 1 )}
-        punjabiTranslation={display.punjabiTranslation && getTranslationFor( 2 )}
-        spanishTranslation={display.spanishTranslation && getTranslationFor( 3 )}
-        englishTransliteration={display.englishTransliteration && getTransliterationFor( 1 )}
-        hindiTransliteration={display.hindiTransliteration && getTransliterationFor( 4 )}
-        urduTransliteration={display.urduTransliteration && getTransliterationFor( 5 )}
-        simpleGraphics={simple}
-      />
-      )}
-      <div className={classNames( { dim }, 'next-lines' )}>
-        {line && nextLines.map( ( { id, gurmukhi } ) => (
-          <Line
-            key={id}
-            className="next-line"
-            simpleGraphics={simple}
-            {...layout}
-            {...display}
-            {...vishraams}
-            gurmukhi={gurmukhi}
-          />
-        ) )}
-      </div>
-    </div>
+
+    </GlobalHotKeys>
   )
 }
 
