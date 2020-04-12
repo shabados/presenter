5818a16204ba87459f52aeb5de8afc130cccfdf1
saihaj <saihajpreet.singh@gmail.com>
2020-04-12 17:54:07 -0500

    refactor: organizes the code and  makes minor style changes

--- app/frontend/src/Controller/ShabadInfo.js
+++ app/frontend/src/Controller/ShabadInfo.js
@@ -12,7 +12,8 @@ import { stripPauses } from '../lib/utils'
 
 import './ShabadInfo.css'
 
-let isOpen = false
+let isPopoverOpen = false
+
 const CopyShabadButton = ( { copyText, onClick: originalOnClick, ...props } ) => {
   const copyToClipboard = useCopyToClipboard()
   const onClick = () => {
@@ -21,7 +22,7 @@ const CopyShabadButton = ( { copyText, onClick: originalOnClick, ...props } ) =>
   }
 
   return (
-    <Tooltip title="Click to copy current shabad">
+    <Tooltip title="Click to copy this shabad">
       <Button className="copy-shabad-button" size="small" {...props} onClick={onClick}>
         Copy Shabad
       </Button>
@@ -46,36 +47,50 @@ CopyShabadButton.defaultProps = {
  */
 const copyShabadText = shabadLines => {
   let shabadText = ''
-  for ( let i = 0; i < shabadLines.length; i += 1 ) {
+  for ( let i = 0; i < shabadLines.length; i++ ) {
     shabadText += shabadLines[ i ].gurmukhi
   }
   return stripPauses( toUnicode( shabadText ) )
 }
 
-export default function ShabadInfo() {
+const dbViewerButton = urlForLine => (
+  <Tooltip title="Report a mistake">
+    <Button
+      className="db-viewer-button"
+      size="small"
+      onClick={() => controller.openExternalUrl( urlForLine )}
+    >
+              Open in DB Viewer
+    </Button>
+  </Tooltip>
+)
+
+const ShabadInfo = () => {
   const [ anchorEl, setAnchorEl ] = React.useState( null )
+  const open = Boolean( anchorEl )
+  const id = open ? 'simple-popover' : undefined
+  const [ line ] = useCurrentLine()
+  const { shabad, bani } = useContext( ContentContext )
+  const writers = useContext( WritersContext )
+  const recommendedSources = useContext( RecommendedSourcesContext )
 
   const handleClick = event => {
-    isOpen = true
+    isPopoverOpen = true
     setAnchorEl( event.currentTarget )
   }
 
   const handleClose = () => {
-    isOpen = false
+    isPopoverOpen = false
     setAnchorEl( null )
   }
 
+  // Change icon
   const checkState = () => {
-    if ( isOpen ) { return faTimesCircle }
+    if ( isPopoverOpen ) { return faTimesCircle }
     return faInfoCircle
   }
-  const open = Boolean( anchorEl )
-  const id = open ? 'simple-popover' : undefined
-  const [ line ] = useCurrentLine()
-  // Get Shabad, writer, sources for getting the author
-  const { shabad, bani } = useContext( ContentContext )
-  const writers = useContext( WritersContext )
-  const recommendedSources = useContext( RecommendedSourcesContext )
+
+  // Get Shabad, writer, sources and db-viewer url
   const getInfo = () => {
     if ( !line ) return ''
     const { nameEnglish: sectionName } = bani || shabad.section
@@ -86,7 +101,9 @@ export default function ShabadInfo() {
     const dbViewerUrl = `https://database.shabados.com/line/${lineId}`
     return [ writerName, sourceName, pageName, sourcePage, sectionName, dbViewerUrl ]
   }
+
   const [ writerName, sourceName, pageName, sourcePage, sectionName, dbViewerUrl ] = getInfo()
+
   return (
     <span>
       <IconButton
@@ -120,16 +137,12 @@ export default function ShabadInfo() {
           <br />
           {writerName}
           <br />
-          <Button
-            className="db-viewer-button"
-            size="small"
-            onClick={() => controller.openExternalUrl( dbViewerUrl )}
-          >
-              Open in DB Viewer
-          </Button>
+          {dbViewerButton( dbViewerUrl )}
           { shabad && ( <CopyShabadButton copyText={copyShabadText( shabad.lines )} /> ) }
         </Typography>
       </Popover>
     </span>
   )
 }
+
+export default ShabadInfo
