87139d7cc5988d04eb1e47a08045dc9810471f65
saihaj <44710980+saihaj@users.noreply.github.com>
2020-05-14 12:18:41 -0500

    fix(frontend): rename "show metadata" option and rearrange the order

--- app/frontend/src/lib/options.js
+++ app/frontend/src/lib/options.js
@@ -120,7 +120,7 @@ export const OPTIONS = {
   disconnectionEvents: { name: 'Disconnections', icon: faPowerOff, type: OPTION_TYPES.toggle, privacy: PRIVACY_TYPES.global },
   downloadEvents: { name: 'Update Download', icon: faDownload, type: OPTION_TYPES.toggle, privacy: PRIVACY_TYPES.global },
   downloadedEvents: { name: 'Update Download Complete', icon: faServer, type: OPTION_TYPES.toggle, privacy: PRIVACY_TYPES.global },
-  showResultMetadata: { name: 'Show Metadata', icon: faTags, type: OPTION_TYPES.toggle, privacy: PRIVACY_TYPES.local },
+  showResultCaptions: { name: 'Show Captions', icon: faTags, type: OPTION_TYPES.toggle, privacy: PRIVACY_TYPES.local },
   resultTranslationLanguage: {
     name: 'Translation',
     icon: faClosedCaptioning,
@@ -253,9 +253,9 @@ export const DEFAULT_OPTIONS = {
       private: false,
     },
     search: {
-      showResultMetadata: false,
       resultTranslationLanguage: OPTIONS.resultTranslationLanguage.values[ 0 ].value,
       resultTransliterationLanguage: OPTIONS.resultTransliterationLanguage.values[ 0 ].value,
+      showResultCaptions: false,
     },
   },
   // Special serverside settings
