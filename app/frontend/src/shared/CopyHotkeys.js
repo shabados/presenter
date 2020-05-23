b79ab74548e78f9ea6cbc15b8e3088b6dcb008bc
Harjot Singh <harjot@harkul.com>
2020-05-23 20:55:50 +0100

    feat(frontend/presenter): add copy all lines shortcut functionality

--- app/frontend/src/shared/CopyHotkeys.js
+++ app/frontend/src/shared/CopyHotkeys.js
@@ -7,11 +7,12 @@ import { mapValues } from 'lodash'
 import { SettingsContext, ContentContext, WritersContext, RecommendedSourcesContext } from '../lib/contexts'
 import { mapPlatformKeys, customiseLine } from '../lib/utils'
 import { COPY_SHORTCUTS } from '../lib/keyMap'
-import { useCopyToClipboard, useCurrentLine, useTranslations, useTransliterations } from '../lib/hooks'
+import { useCopyToClipboard, useCurrentLine, useTranslations, useTransliterations, useCurrentLines } from '../lib/hooks'
 import { LANGUAGES } from '../lib/consts'
 
 const CopyHotkeys = ( { children } ) => {
   const { local: { hotkeys, display: { lineEnding } } } = useContext( SettingsContext )
+  const lines = useCurrentLines()
   const [ line ] = useCurrentLine()
   const { typeId } = line || {}
 
@@ -52,12 +53,16 @@ const CopyHotkeys = ( { children } ) => {
     return `${writerName} - ${sourceName} - ${pageName} ${sourcePage}`
   }
 
+  const getAllLines = () => lines.map( ( { gurmukhi } ) => gurmukhi ).join( ' ' )
+
   const copyToClipboard = useCopyToClipboard()
 
   // Generate hotkeys for copying to clipboard
   const hotkeyHandlers = [
     [ COPY_SHORTCUTS.copyGurmukhiAscii.name, () => stripVishraams( line.gurmukhi ), 'gurmukhi' ],
     [ COPY_SHORTCUTS.copyGurmukhiUnicode.name, () => stripVishraams( toUnicode( line.gurmukhi ) ), 'gurmukhi' ],
+    [ COPY_SHORTCUTS.copyAllLinesAscii.name, () => stripVishraams( getAllLines() ), 'lines' ],
+    [ COPY_SHORTCUTS.copyAllLinesUnicode.name, () => stripVishraams( toUnicode( getAllLines() ) ), 'lines' ],
     [ COPY_SHORTCUTS.copyEnglishTranslation.name, () => translations.english, 'english translation' ],
     [ COPY_SHORTCUTS.copyPunjabiTranslation.name, () => translations.punjabi, 'punjabi translation' ],
     [ COPY_SHORTCUTS.copySpanishTranslation.name, () => translations.spanish, 'spanish translation' ],
