6ddc8410d84a192b1277fc068f132e741420cb09
saihaj <44710980+saihaj@users.noreply.github.com>
2020-05-25 12:58:43 -0500

    fix(frontend/settings): use throttle instead od debounce

--- app/frontend/src/Settings/DynamicOptions.js
+++ app/frontend/src/Settings/DynamicOptions.js
@@ -3,7 +3,7 @@ import { string, shape, node, bool } from 'prop-types'
 
 import { Typography, Grid } from '@material-ui/core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { debounce } from 'lodash'
+import { throttle } from 'lodash'
 
 import { OPTIONS, DEFAULT_OPTIONS, PRIVACY_TYPES, FLAT_OPTION_GROUPS } from '../lib/options'
 import controller from '../lib/controller'
@@ -103,7 +103,7 @@ const DynamicOptions = ( { device, group } ) => {
               {...props}
               option={option}
               value={value}
-              onChange={debounce( setSettings, 100, { leading: true } )}
+              onChange={throttle( setSettings, 100, { leading: true } )}
               disabled={isDisabled}
             />
           </OptionSlot>
