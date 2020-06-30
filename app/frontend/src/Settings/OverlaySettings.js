b36965da5657b5059f24f7b65c32ae4054c583e8
saihaj <44710980+saihaj@users.noreply.github.com>
2020-06-30 13:18:33 -0500

    fix(frontend/settings): change theme tool button position

--- app/frontend/src/Settings/OverlaySettings.js
+++ app/frontend/src/Settings/OverlaySettings.js
@@ -45,16 +45,17 @@ const OverlaySettings = () => {
 
       <OptionGrid container align="center">
         <Grid item {...slotSizes.single}>
-
-          <Grid container justify="space-around">
-            <TutorialButton className="tutorial-button" href="https://tutorials.shabados.com/tutorials/1.0.0/overlay/overlay.html">
+          <TutorialButton className="tutorial-button" href="https://tutorials.shabados.com/tutorials/1.0.0/overlay/overlay.html">
             Learn More
-            </TutorialButton>
-            <TutorialButton className="theme-tool" href="https://themes.shabados.com">
-            Theme Tool
-            </TutorialButton>
-          </Grid>
+          </TutorialButton>
+        </Grid>
+      </OptionGrid>
 
+      <OptionGrid container align="center">
+        <Grid item {...slotSizes.single}>
+          <TutorialButton className="theme-tool" href="https://themes.shabados.com">
+            Theme Tool
+          </TutorialButton>
         </Grid>
       </OptionGrid>
 
