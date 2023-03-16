1cf64133fab7a0bf64c9bd22dd5574deab649a4f
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2023-03-16 23:44:27 +0000

    refactor: update Jest & TS config to work with JSX files

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,16 @@
-/// jest.d.ts
-
 module.exports = {
-  projects: [ '<rootDir>/apps/*', '<rootDir>/packages/*' ],
+  projects: ['<rootDir>/apps/*', '<rootDir>/packages/*'],
   transform: {
-    '^.+\\.(t|j)sx?$': [ '@swc/jest' ],
+    '^.+\\.(t|j)sx?$': '@swc/jest',
   },
-  resetMocks: true,
-}
+  testEnvironment: 'jsdom',
+  // resetMocks: true,
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+};
