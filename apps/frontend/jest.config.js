71fa6c5d8d7e1761a5bedda86ffd6c4fc683be65
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2023-03-23 17:21:43 +0000

    build: add Jest DOM packages for testing

--- apps/frontend/jest.config.js
+++ apps/frontend/jest.config.js
@@ -1,6 +1,10 @@
-const base = require( '../../jest.config' )
+const base = require('../../jest.config')
 
 module.exports = {
   ...base,
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: [
+    '<rootDir>/apps/frontend/setupTests.ts',
+    // '<rootDir>/setupTests.ts',
+  ],
 }
