41aa8ca00376c043895354af558be439657cb3be
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2023-03-23 17:22:59 +0000

    refactor: make name an optional prop for ThemeLoader
    Remove the mandatory setting for name because there's a default value provided

--- apps/frontend/src/shared/ThemeLoader.tsx
+++ apps/frontend/src/shared/ThemeLoader.tsx
@@ -4,7 +4,7 @@ import { THEMES_URL } from '../lib/consts'
 import { StatusContext } from '../lib/contexts'
 import defaultTheme from '../Presenter/themes/Day.css'
 
-type ThemeLoaderProps = { name: string }
+type ThemeLoaderProps = { name?: string }
 
 /**
  * Component to load a theme using a `<link>` tag.
@@ -16,7 +16,7 @@ const ThemeLoader = ( { name = 'Day' }: ThemeLoaderProps ) => {
 
   return (
     <link
-      key={`${name}-${connectedAt?.toDateString() || ''}`}
+      key={`${name}-${connectedAt}`}
       rel="stylesheet"
       href={name ? `${THEMES_URL}/${name}.css` : defaultTheme}
     />
