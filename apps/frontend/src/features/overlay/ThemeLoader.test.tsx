56486e0b1c822c8a7e8dbe2afde492b32cc31323
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2023-03-24 17:31:28 +0000

    test: add ThemeLoader tests

---
+++ apps/frontend/src/features/overlay/ThemeLoader.test.tsx
@@ -0,0 +1,43 @@
+import { render } from '@testing-library/react'
+
+import { StatusContext } from '../lib/contexts'
+import ThemeLoader from '../../components/ThemeLoader'
+
+jest.mock( '../lib/consts', () => ( {
+  isMac: false,
+} ) )
+
+describe( 'ThemeLoader', () => {
+  it( 'should generate a link element with a href of the default Day.css value', () => {
+    const { container } = render(
+      <StatusContext.Provider
+        value={{ connected: false, connectedAt: null, status: null }}
+      >
+        <ThemeLoader />
+      </StatusContext.Provider>
+    )
+
+    const link = container.querySelector( 'link' )
+
+    expect( link ).toBeInTheDocument()
+    expect( link?.getAttribute( 'href' ) ).toMatch( /Day.css/ )
+  } )
+
+  it(
+    'should generate a link element with a href of the given name value',
+    () => {
+      const { container } = render(
+        <StatusContext.Provider
+          value={{ connected: false, connectedAt: null, status: null }}
+        >
+          <ThemeLoader name="Night" />
+        </StatusContext.Provider>
+      )
+
+      const link = container.querySelector( 'link' )
+
+      expect( link ).toBeInTheDocument()
+      expect( link?.getAttribute( 'href' ) ).toMatch( /Night.css/ )
+    }
+  )
+} )
