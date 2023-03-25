678551510016018fdc70b8202a92d0ffbbd45480
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2023-03-25 08:24:27 +0000

    test: change generic consts mock to mock detect() from detect-browser

--- apps/frontend/setupTests.ts
+++ apps/frontend/setupTests.ts
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom'
 
-jest.mock( './src/helpers/consts' )
+jest.mock( 'detect-browser' )
 
 global.fetch = jest.fn().mockResolvedValue( {
   json: () => Promise.resolve( {} ),
