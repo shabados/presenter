88895c7756fce2e6e96334dfc64795134479c44e
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2023-03-29 06:50:12 +0100

    refactor: replace useHistory() with useNavigate() in Presenter index

--- apps/frontend/src/Presenter/index.js
+++ apps/frontend/src/Presenter/index.js
@@ -8,7 +8,7 @@ import classNames from 'classnames'
 import queryString from 'qs'
 import { lazy, Suspense, useContext, useRef, useState } from 'react'
 import IdleTimer from 'react-idle-timer'
-import { Route, useHistory, useLocation } from 'react-router-dom'
+import { Route, useLocation, useNavigate } from 'react-router-dom'
 
 import {
   BOOKMARKS_URL,
@@ -53,7 +53,7 @@ const DEFAULT_IDLE_EVENTS = [
 ]
 
 const Presenter = () => {
-  const history = useHistory()
+  const navigate = useNavigate()
   const location = useLocation()
   const { search, pathname } = location
   const { controllerOnly } = getUrlState( search )
@@ -78,7 +78,7 @@ const Presenter = () => {
    * Sets the query string parameters, retaining any currently present.
    * @param params The query string parameters.
    */
-  const setQueryParams = ( params ) => history.push( {
+  const setQueryParams = ( params ) => navigate( {
     ...location,
     search: queryString.stringify( { ...getUrlState( search ), ...params } ),
   } )
@@ -87,7 +87,7 @@ const Presenter = () => {
    * More concise form to navigate to URLs, retaining query params.
    * @param pathname The path to navigate to.
    */
-  const go = ( pathname ) => history.push( { ...location, pathname } )
+  const go = ( pathname ) => navigate( { ...location, pathname } )
 
   /**
    * Toggles the controller.
@@ -100,7 +100,7 @@ const Presenter = () => {
   /**
    * Always puts the controller in fullscreen.
    */
-  const setFullscreenController = () => history.push( {
+  const setFullscreenController = () => navigate( {
     pathname: CONTROLLER_URL,
     search: queryString.stringify( { [ STATES.controllerOnly ]: true } ),
   } )
