282512a88938692ec9a69fffb138108f06b5844a
Bhajneet S.K <bhajneet@gmail.com>
2023-04-02 19:22:11 -0400

    refactor: use existing method for handling vishraam lines

--- app/frontend/src/ScreenReader/index.js
+++ app/frontend/src/ScreenReader/index.js
@@ -3,19 +3,67 @@ import { hot } from 'react-hot-loader/root'
 
 import { ContentContext } from '../lib/contexts'
 
+import { classifyWords } from '../lib/line'
 import './index.css'
 
-const VishraamJsx = word => {
-  switch ( word.slice( -1 ) ) {
-    case ';':
-      return <span className="heavy">{word.slice( 0, -1 )}</span>
-    case ',':
-      return <span className="medium">{word.slice( 0, -1 )}</span>
-    case '.':
-      return <span className="light">{word.slice( 0, -1 )}</span>
-    default:
-      return <>{word}</>
+const titlesFuzzy = [
+  '<>',
+  '] jpu ]',
+  'pwiqswhI 10',
+  '] qÍ pRswid ]',
+  '] qÍ pRswid kQqy ]',
+  'BujMg pRXwq CMd ]',
+  '] cwcrI CMd ]',
+  '] sv`Xy ]',
+  '] cOpeI ]',
+  'mhlw 1',
+  'mhlw 2',
+  'mhlw 3',
+  'mhlw 4',
+  'mhlw 5',
+  'mhlw 6',
+  'mhlw 7',
+  'mhlw 8',
+  'mhlw 9',
+  'mÚ 1',
+  'mÚ 2',
+  'mÚ 3',
+  'mÚ 4',
+  'mÚ 5',
+  'mÚ 6',
+  'mÚ 7',
+  'mÚ 8',
+  'mÚ 9',
+]
+
+const titlesExact = [
+  'jwpu ]',
+  'sloku ]',
+  'pauVI ]',
+  'AstpdI ]',
+  'cwcrI CMd ]',
+  'eyk ACrI CMd ]',
+  'sÍYXw ]',
+  'dohrw ]',
+  'sRI BgauqI jI shwie ]',
+  'vwr sRI BgauqI jI kI ]',
+  'rhrwis swihb',
+]
+
+const isTitle = str => {
+  if ( titlesFuzzy.some( subtitle => str.indexOf( subtitle ) >= 0 )
+    || titlesExact.some( title => title === str ) ) {
+    return 'title'
   }
+  return ''
+}
+
+const isEndOfPauri = str => {
+  // if there is a line ending (॥੧॥) or ardas
+  if ( /][\d]+]/.test( str ) || str.indexOf( 'bolo jI vwihgurU [' ) >= 0 ) {
+    return 'end-of-pauri'
+  }
+  return ''
 }
 
 const ScreenReader = () => {
@@ -23,78 +71,17 @@ const ScreenReader = () => {
 
   const { lines = [] } = shabad || bani || {}
 
-  const titlesFuzzy = [
-    '<>',
-    '] jpu ]',
-    'pwiqswhI 10',
-    '] qÍ pRswid ]',
-    '] qÍ pRswid kQqy ]',
-    'BujMg pRXwq CMd ]',
-    '] cwcrI CMd ]',
-    '] sv`Xy ]',
-    '] cOpeI ]',
-    'mhlw 1',
-    'mhlw 2',
-    'mhlw 3',
-    'mhlw 4',
-    'mhlw 5',
-    'mhlw 6',
-    'mhlw 7',
-    'mhlw 8',
-    'mhlw 9',
-    'mÚ 1',
-    'mÚ 2',
-    'mÚ 3',
-    'mÚ 4',
-    'mÚ 5',
-    'mÚ 6',
-    'mÚ 7',
-    'mÚ 8',
-    'mÚ 9',
-  ]
-  const titlesExact = [
-    'jwpu ]',
-    'sloku ]',
-    'pauVI ]',
-    'AstpdI ]',
-    'cwcrI CMd ]',
-    'eyk ACrI CMd ]',
-    'sÍYXw ]',
-    'dohrw ]',
-    'sRI BgauqI jI shwie ]',
-    'vwr sRI BgauqI jI kI ]',
-    'rhrwis swihb',
-  ]
-
   return (
     <div className="lines-container">
       <div className="lines">
-        {lines.map( ( { gurmukhi, id } ) => {
-          const isTitle = titlesFuzzy.some( ele => gurmukhi.indexOf( ele ) >= 0 )
-            || titlesExact.some( ele => ele === gurmukhi )
-
-          const isEndOfPauri = /][\d]+]/.test( gurmukhi )
-            || gurmukhi.indexOf( 'bolo jI vwihgurU [' ) >= 0
-
-          return (
-            <p
-              key={id}
-              className={`line ${isTitle ? 'title' : ''}${
-                isEndOfPauri ? 'end-of-pauri' : ''
-              }`}
-            >
-              {gurmukhi.split( ' ' ).map( ( word, index ) => {
-                const betweenWords = index === 0 ? '' : ' '
-                return (
-                  <>
-                    {betweenWords}
-                    {VishraamJsx( word )}
-                  </>
-                )
-              } )}
-            </p>
-          )
-        } )}
+        {lines.map( ( { gurmukhi, id } ) => (
+          <p
+            key={id}
+            className={`line ${isTitle( gurmukhi )} ${isEndOfPauri( gurmukhi )}`}
+          >
+            {classifyWords( gurmukhi ).map( ( { word, type } ) => <span key={`${word}-${type}`} className={`word ${type}`}>{word}</span> )}
+          </p>
+        ) )}
       </div>
     </div>
   )
