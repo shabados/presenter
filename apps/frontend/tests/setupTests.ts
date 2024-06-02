19df310d784d8484d6cd430bc3f9310b50dd23e4
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2024-06-02 19:02:33 +0100

    test: mute unnecessary happy-dom abort errors on teardown

--- apps/frontend/tests/setupTests.ts
+++ apps/frontend/tests/setupTests.ts
@@ -1,8 +1,19 @@
 import '@testing-library/jest-dom/vitest'
 
-import { vi } from 'vitest'
+import { afterEach, beforeEach, vi } from 'vitest'
 
 vi.mock( 'detect-browser' )
 vi.mock( 'notistack' )
 
 global.fetch = vi.fn()
+
+const actualConsole = console.error
+
+// We do not want to know about any errors that occur during teardown
+afterEach( () => {
+  console.error = vi.fn()
+} )
+
+beforeEach( () => {
+  console.error = actualConsole
+} )
