1be8d0a6028ce971bf8e0104485a8c8f5ba03929
Harjot Singh <harjot@harkul.com>
2024-08-05 19:43:58 +0100

    fix(frontend): load default theme in vite

--- apps/frontend/src/shared/ThemeLoader.tsx
+++ apps/frontend/src/shared/ThemeLoader.tsx
@@ -1,16 +1,11 @@
 import { useContext } from 'react'
 
-import { THEMES_URL } from '../lib/consts'
-import { StatusContext } from '../lib/contexts'
-import defaultTheme from '../Presenter/themes/Day.css'
+import { THEMES_URL } from '~/lib/consts'
+import { StatusContext } from '~/lib/contexts'
+import defaultTheme from '~/Presenter/themes/Day.css?url'
 
 type ThemeLoaderProps = { name: string }
 
-/**
- * Component to load a theme using a `<link>` tag.
- * @param {string} name The name of the CSS theme to load from the server.
- * @constructor
- */
 const ThemeLoader = ( { name = 'Day' }: ThemeLoaderProps ) => {
   const { connectedAt } = useContext( StatusContext )
 
