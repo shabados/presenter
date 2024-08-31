ed6462a2f599e58306295c058f1ebee91f60eeef
Harjot Singh <harjot@harkul.com>
2024-08-31 15:51:49 +0100

    fix: do not import Day theme by default
    Temporary!

--- app/frontend/src/shared/ThemeLoader.js
+++ app/frontend/src/shared/ThemeLoader.js
@@ -4,8 +4,6 @@ import { string, instanceOf } from 'prop-types'
 import { THEMES_URL } from '../lib/consts'
 import { withContext, StatusContext } from '../lib/contexts'
 
-import defaultTheme from '../Presenter/themes/Day.css'
-
 /**
  * Component to load a theme using a `<link>` tag.
  * @param {string} name The name of the CSS theme to load from the server.
@@ -15,7 +13,7 @@ const ThemeLoader = ( { name, connectedAt } ) => (
   <link
     key={`${name}-${connectedAt}`}
     rel="stylesheet"
-    href={name ? `${THEMES_URL}/${name}.css` : defaultTheme}
+    href={`${THEMES_URL}/${name}.css`}
   />
 )
 
