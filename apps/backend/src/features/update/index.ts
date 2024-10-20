f513010d47c0e56ca2fa735f34be7c10fdaa2691
Manpreet Singh <ManpreetSL@users.noreply.github.com>
2024-10-20 03:28:14 +0100

    feat(backend): replace hardcoded version numbers to values from the relevant JSON files

--- apps/backend/src/features/update/index.ts
+++ apps/backend/src/features/update/index.ts
@@ -1,11 +1,38 @@
+import { join } from 'node:path'
+
+import { getLogger, UPDATES_FILE } from '@presenter/node'
+import type { PackageJson } from 'type-fest'
+
+import { APP_FOLDER } from '~/helpers/consts'
+import { readJSON } from '~/helpers/files'
 import { ExpressApi } from '~/services/express'
+
+const log = getLogger( 'diagnostics' )
 
 type UpdatesModuleOptions = {
   api: ExpressApi['api'],
 }
 
+type UpdatesJsonType = {
+  prevVersion: string,
+}
+
 const createUpdatesModule = async ( { api }: UpdatesModuleOptions ) => {
-  api.get( '/update', ( _, res ) => res.json( { prevVersion: '1.1.1', currVersion: '2.2.2' } ) )
+  api.get( '/update', ( _, res ) => void Promise.all(
+    [
+      readJSON<PackageJson>( join( APP_FOLDER, 'package.json' ) ),
+      readJSON<UpdatesJsonType>( UPDATES_FILE ),
+    ]
+  )
+    .then( ( [ { version: currVersion },
+      { prevVersion },
+    ] ) => res.json( {
+      prevVersion,
+      currVersion,
+    } ) )
+    .catch( ( err ) => {
+      log.error( err, 'Failed to read version JSON' )
+    } ) )
 }
 
 export default createUpdatesModule
